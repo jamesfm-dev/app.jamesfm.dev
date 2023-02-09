68bc2ce1c5440acf35fd980ea2cf8f822037d064
James M <66410095+jamesfm-dev@users.noreply.github.com>
2023-02-09 23:33:28 +0800

    :bug: Fix Form max width sizing

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -4,23 +4,25 @@ import FormSubmit from "./FormSubmit";
 
 export default function Form() {
   return (
-    <form className="max-w-xs mx-auto my-16 p-4 rounded-lg shadow-lg bg-white">
-      <div className="ml-1 mb-4">
-        <FormHeader
-          headline="Master account login"
-          subheadline={
-            <>
-              To use demo account, fill user/pass with &#34;
-              <span className="font-bold">demo</span>&#34;.
-            </>
-          }
-        />
-      </div>
-      <div className="grid grid-cols-1 gap-2">
-        <FormInput type="text" isRequired placeholder="Username" />
-        <FormInput type="password" isRequired placeholder="Password" />
-      </div>
-      <FormSubmit />
-    </form>
+    <section>
+      <form className="max-w-sm mx-auto my-16 p-4 rounded-lg shadow-lg bg-white">
+        <div className="ml-1 mb-4">
+          <FormHeader
+            headline="Master account login"
+            subheadline={
+              <>
+                To use demo account, fill user/pass with &#34;
+                <span className="font-bold">demo</span>&#34;.
+              </>
+            }
+          />
+        </div>
+        <div className="grid grid-cols-1 gap-2">
+          <FormInput type="text" isRequired placeholder="Username" />
+          <FormInput type="password" isRequired placeholder="Password" />
+        </div>
+        <FormSubmit />
+      </form>
+    </section>
   );
 }
