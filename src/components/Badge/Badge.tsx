9914bc0b3913981194cc04fcd17ac183b6d4fe1d
James M <66410095+jamesfm-dev@users.noreply.github.com>
2023-02-09 23:36:58 +0800

    :lipstick: Add hover styling on Badge ui

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -7,7 +7,7 @@ export default function Badge({
 }) {
   return (
     <span
-      className={`px-[4px] py-[2px] text-xs rounded-full shadow-md ${color}`}
+      className={`px-[4px] py-[2px] text-xs rounded-full shadow-md hover:bg-opacity-75 hover:cursor-pointer ${color}`}
     >
       {label}
     </span>
