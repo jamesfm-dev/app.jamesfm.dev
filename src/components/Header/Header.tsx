f563e503c3e903a83a2494cb5b1834df17d7becd
James M <66410095+jamesfm-dev@users.noreply.github.com>
2023-02-10 01:21:43 +0800

    :lipstick: Change Header background color to white

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import HeaderLogo from "./HeaderLogo";
 
 export default function Header() {
   return (
-    <header className="h-auto flex justify-center items-center bg-transparent">
+    <header className="h-auto flex justify-center items-center bg-white shadow-md">
       <nav className="container mx-auto px-4 my-4 md:text-left text-center">
         <HeaderLogo href="https://app.jamesfm.dev" label="app.jamesfm.dev" />
       </nav>
