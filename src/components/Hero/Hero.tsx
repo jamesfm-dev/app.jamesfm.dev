2c7edf4816311b7d8b8c36aa5b0802a1ba7435aa
James M <66410095+jamesfm-dev@users.noreply.github.com>
2023-02-11 02:58:41 +0800

    :art: Remove container class on Hero section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,7 +3,7 @@ import Intro from "../Intro";
 
 export default function Hero() {
   return (
-    <div className="container mx-auto grid md:grid-cols-2">
+    <div className="grid md:grid-cols-2">
       <Intro />
       <Form />
     </div>
